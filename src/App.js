1ba8bc20e4779048975e5b462cdb64d921579726
Art <adrasi@gmail.com>
2022-11-15 13:36:26 +0000

    added docker compose, made changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
+import '@testing-library/jest-dom'
 
 function App() {
-  const { version } = window['appConfig'] || 'demo';
+  const version = window['appConfig']?.version || 'demo';
 
   return (
     <div className="App">
